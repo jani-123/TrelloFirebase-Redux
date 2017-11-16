bcc2fe63ef075bc7a3eb5ab98f0e099859fff4c2
jani-123 <janina.rqa@gmail.com>
2017-11-16 12:59:02 -0500

    poniendo vistas

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,15 +1,53 @@
-import React from 'react'
+import React from "react";
 //import {addStage} from '../../actions/actions'
 //import Stage from '../Stage/Stage';
-//import './Board.css'
+import trello from '../../trello-logo.png';
+import './SignIn.css'
 import { NavLink } from "react-router-dom";
-const SignIn = ({title}) => {
-   return ( 
-      <div className = "Board-container">
-        <h1>{title}</h1>
-        <NavLink to={'/SignUp'}>Next</NavLink>
+const SignIn = ({ title }) => {
+  return (
+    <div className="sign">
+      <img src={trello} className="logo2" />
+      <form className="form-horizontal formulario">
+        <div className="form-group">
+          <div className="col-sm-10">
+            <input
+              type="email"
+              className="form-control texto"
+              placeholder="Email"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-10">
+            <input
+              type="password"
+              className="form-control texto"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-offset-2 col-sm-10">
+            <NavLink to="/Borad" type="submit" className="btn btn-lg btn-sign-in">
+              Sign in
+            </NavLink>
+          </div>
+        </div>
+      </form>
+      <div className="new-vist">
+        <NavLink to="/SignUp" type="submit" className="btn new">
+          Create new account
+        </NavLink>
       </div>
-   ); 
-}
+      <div className="footer">
+        <a href="https://trello.com" className="new">
+          Trello
+        </a>{" "}
+        tribute for educational purposes crafted with ♥ for Chio by @ChioQA
+      </div>
+    </div>
+  );
+};
 
 export default SignIn;
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,14 +1,80 @@
-import React from 'react'
+import React from "react";
+import trello from '../../trello-logo.png';
 //import {addStage} from '../../actions/actions'
-//import Stage from '../Stage/Stage';
-//import './Board.css'
+import './SignUp.css'
+import { NavLink } from "react-router-dom";
 
-const SignUp = ({title}) => {
-   return ( 
-      <div className = "Board-container">
-        <h1>Peru{title}</h1>
+const SignUp = ({ title }) => {
+  return (
+    <div className="sign">
+    <img src={trello} className="logo2" />
+      <form className="form-horizontal formulario">
+        <div className="form-group">
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control texto"
+              placeholder="First name"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control texto"
+              placeholder="Last name"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-10">
+            <input
+              type="email"
+              className="form-control texto"
+              placeholder="Email"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-10">
+            <input
+              type="password"
+              className="form-control texto"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-10">
+            <input
+              type="password"
+              className="form-control texto"
+              placeholder="Confirm password"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-offset-2 col-sm-10">
+            <NavLink to="/Board" type="submit" className="btn btn-lg btn-sign-in">
+              Sign Up
+            </NavLink>
+          </div>
+        </div>
+      </form>
+      <div className="new-vist">
+        <NavLink to="/" type="submit" className="btn new">
+          Sign In
+        </NavLink>
       </div>
-   ); 
-}
+      <div className="footer">
+        <a href="https://trello.com" className="new">
+          Trello
+        </a>{" "}
+        tribute for educational purposes crafted with ♥ for Chio by @ChioQA
+      </div>
+    </div>
+  );
+};
 
 export default SignUp;
