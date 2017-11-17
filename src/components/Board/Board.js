95b37f28893cf57b5608c1c7abefb5c3fb18864e
jani-123 <janina.rqa@gmail.com>
2017-11-17 15:31:53 -0500

    implementa parte del board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,23 +1,47 @@
 import React from "react";
-import {signOut} from '../../actions/actions'
+import { signOut } from "../../actions/actions";
 //import Stage from '../Stage/Stage';
-//import './Board.css'
-import { NavLink , Redirect} from "react-router-dom";
+import "./Board.css";
+import trello from "../../trello-logo.png";
+import { NavLink, Redirect } from "react-router-dom";
 
-const Board = ({ successLogin, user }) => {
-  return <div className="Board-container">
-      {!successLogin && <Redirect to="/SignIn" />}
-      <div>
-        <div>
-          {user.firstname} - {user.lastname} - {user.email} 
-        </div>
-        <div>
-          <button type="button" className="btn new" onClick={signOut}>
-            SignOut
+const NavBoard = ({ user }) => {
+  return (
+    <nav className="navbar">
+      <div className="container-fluid  Narvab">
+        <div className="navbar-header">
+          <button className="btn botones" type="submit">
+            <i className="fa fa-trello fa-2x iconTrello" aria-hidden="true">
+              Tableros
+            </i>
           </button>
         </div>
+        <div className="navbar-header">
+          <img src={trello} className="logo" />
+        </div>
+        <div className="navbar-header">
+          <h4 className="user">
+            <i className="fa fa-user-circle" aria-hidden="true" />  {user.email}  | <button className="btn-link btn-signout" onClick={signOut}>Cerrar Sesión</button>
+          </h4>
+        </div>
       </div>
-    </div>;
+    </nav>
+  );
+};
+
+const Board = ({ successLogin, user }) => {
+  return (
+    <div className="Board-container">
+      {!successLogin && <Redirect to="/SignIn" />}
+      <div>
+        <NavBoard user={user}/>
+        <div>
+          HOLA MUNDO!!
+        </div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default Board;
